b77775847452ea285fb5dd632fb9b23d4e05f18b
L.K. <lenguyenanhkhoa2001@gmail.com>
2025-09-07 20:47:13 +0700

    Update executer.cpp

--- Shell/executer.cpp
+++ Shell/executer.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
-#include "executer.h"
+#include <string>
+#include <filesystem>
+#include "executer.cpp"
 
-void executeCommand(const std::string& command) {
-    if (command == "time") {
-        commands::printTime();
-    } else if (command == "exit") {
-        commands::exitShell();
-    } else if (command == "help") {
-        commands::help();
-    } else if (command == "cls" || command == "clear") {
-        commands::clearScreen();
-        printMessage("Type 'help' to get list of commands.\n");
-    } else if (command == "dir") {
-        commands::getItemsInDirectory(std::filesystem::current_path().string());
-    } else if (command.empty()) {
+void clearScreen() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
 
-    } else {
-        commandError(command);
+int main() {
+    clearScreen();
+    std::cout << "Type 'cmds' to get list of commands.\n";
+
+    while (true) {
+        std::string input;
+        std::filesystem::path currentPath = std::filesystem::current_path();
+        std::cout << currentPath << ":> ";
+
+        if (std::getline(std::cin, input)) {
+            executeCommand(input);
+            continue;
+        }
     }
-    return;
 }
